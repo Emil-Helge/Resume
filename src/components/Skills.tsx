5655615d2326f58d2488444b20fb94fcb22b6519
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-03-12 19:37:30 +0100

    Added keyframes to fill up skill bars

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,31 +1,40 @@
 function Skills() {
+  const skillsData = [
+    { label: "HTML-and-CSS", value: 85 },
+    { label: "UX-and-Usability", value: 78 },
+    { label: "Javascript", value: 75 },
+    { label: "Typescript", value: 71 },
+    { label: "React", value: 51 },
+  ];
+
   return (
     <>
+      <style>
+        {skillsData.map(
+          (skill) => `
+          .${skill.label} {
+            animation: ${skill.label} 2.5s ease-in-out forwards;
+          }
+          @keyframes ${skill.label} {
+            from { width: 0%; }
+            to { width: ${skill.value}% }
+          }
+        `
+        )}
+      </style>
       <div id="Skills" className="flex flex-col text-right pr-5 gap-5">
         <h1 className="text-4xl font-mono font-extrabold mb-2 first-letter:text-[#00d1cd]">
           Skills
         </h1>
-        <p className="text-lg">HTML & CSS</p>
-        <div className="progress-bar-container flex bg-slate-700 border-t-2 border-[#00d1cd] justify-start items-center rounded-xl pl-1 pr-1 h-10 w-60 self-end">
-          <div className="progress-bar h-7 w-0 bg-gray-300 rounded-xl animate-[firstBarWidth_3s_normal_forwards]"></div>
-          <span id="max-value-1" className="ml-1 font-extrabold text-sm">
-            85
-          </span>
-        </div>
-        <p className="text-lg">UX & Usability</p>
-        <div className="progress-bar-container flex bg-slate-700 justify-start items-center rounded-xl pl-1 pr-1 h-10 w-60 self-end">
-          <div className="progress-bar h-7 w-0 bg-gray-300 rounded-xl animate-[secondBarWidth_3s_normal_forwards]"></div>
-          <span id="max-value-2" className="ml-1 font-extrabold text-sm">
-            78
-          </span>
-        </div>
-        <p className="text-lg">Javascript</p>
-        <div className="progress-bar-container flex bg-slate-700 border-b-2 border-[#00d1cd] justify-start items-center rounded-xl pl-1 pr-1 h-10 w-60 self-end">
-          <div className="progress-bar h-7 w-0 bg-gray-300 rounded-xl animate-[thirdBarWidth_2s_normal_forwards]"></div>
-          <span id="max-value-3" className="ml-1 font-extrabold text-sm">
-            36
-          </span>
-        </div>
+        {skillsData.map(({ label, value }) => (
+          <div key={label} className="self-end">
+            <div>{label.replace(/-and-/g, " & ")}</div>
+            <div className="flex bg-slate-700 border-b-2 border-[#00d1cd] justify-start items-center rounded-xl pl-1 pr-1 h-10 w-60 self-end ">
+              <div className={`  h-7 w-0 bg-gray-300 rounded-xl ${label}`} />
+              <span className="ml-1 font-extrabold text-sm">{value}</span>
+            </div>
+          </div>
+        ))}
         <h4 className="text-2xl font-bold">Additional proficiencies include</h4>
         <p className="text-lg">
           Editing images with Gimp
