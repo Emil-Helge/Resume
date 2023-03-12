67ffaf41adf377d8d093f53b4890a0a334b2c887
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-03-12 15:51:11 +0100

    added `backToTop`-button

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+
 function Education() {
+  const [showBackToTop, setShowBackToTop] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 500) {
+        setShowBackToTop(true);
+      } else {
+        setShowBackToTop(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleBackToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
       <div id="Education" className="flex flex-col text-left pl-5 gap-5">
@@ -15,6 +38,14 @@ function Education() {
             <p>2022-2024</p>
           </div>
         </div>
+        {showBackToTop && (
+          <button
+            onClick={handleBackToTop}
+            className="fixed xl:bottom-16 xl:right-10 bottom-0 right-0 p-5 opacity-50 hover:opacity-100 hover:text-[#00d1cd] transition duration-500 ease-in-out transform hover:animate-wiggle"
+          >
+            <i className="fa-solid fa-arrow-up fa-xl"></i>
+          </button>
+        )}
       </div>
       <hr className="mt-11 mb-11 w-1/3 scroll-animation-hidden border-[#00d1cd]" />
     </>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: ["hover"],
+    },
+    keyframes: {
+      wiggle: {
+        "0%, 100%": { transform: "translateY(0)" },
+        "50%": { transform: "translateY(-.5rem)" },
+      },
+    },
+    animation: {
+      wiggle: "wiggle 1s ease-in-out infinite",
+    },
   },
   plugins: [],
 };
