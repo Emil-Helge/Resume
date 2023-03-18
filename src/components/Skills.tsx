75cf443a20279e862619d97ff21c104747ee7b55
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-03-18 19:12:35 +0100

    Skill-bars only start filling up when in view

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
 import HrAnimation from './HrAnimation';
+import useInView from './useInView';
 
 function Skills() {
+  const { ref, inView } = useInView(0.4);
+
+  const [animationApplied, setAnimationApplied] = useState(false);
+
+  useEffect(() => {
+    if (inView && !animationApplied) {
+      setAnimationApplied(true);
+    }
+  }, [inView, animationApplied]);
+
   const skillsData = [
     { label: 'HTML-and-CSS', value: 95 },
     { label: 'UX-and-Usability', value: 85 },
@@ -28,24 +40,34 @@ function Skills() {
   return (
     <>
       <style>{keyframes}</style>
-      <div id="Skills" className="flex flex-col text-right pr-5 gap-5">
-        <h1 className="text-4xl font-mono font-extrabold mb-2 first-letter:text-[#00d1cd]">
-          Skills
-        </h1>
-        {skillsData.map(({ label }) => (
-          <div key={label} className="self-end">
-            <div>{label.replace(/-and-/g, ' & ')}</div>
-            <div className="flex bg-slate-700 border-r-[0.1rem] border-[#00d1cd] rounded-lg h-10 w-60">
-              <div className={`  h-10  bg-gray-300 rounded-lg ${label}`} />
+      <div ref={ref}>
+        {' '}
+        {/* Attach the ref to this wrapping div */}
+        <div id="Skills" className="flex flex-col text-right pr-5 gap-5">
+          <h1 className="text-4xl font-mono font-extrabold mb-2 first-letter:text-[#00d1cd]">
+            Skills
+          </h1>
+          {skillsData.map(({ label }) => (
+            <div key={label} className="self-end">
+              <div>{label.replace(/-and-/g, ' & ')}</div>
+              <div className="flex bg-slate-700 border-r-[0.1rem] border-[#00d1cd] rounded-lg h-10 w-60">
+                <div
+                  className={`  h-10  bg-gray-300 rounded-lg ${
+                    animationApplied ? label : ''
+                  }`}
+                />
+              </div>
             </div>
-          </div>
-        ))}
-        <h4 className="text-2xl font-bold">Additional proficiencies include</h4>
-        <p className="text-lg">
-          Editing images with Gimp
-          <br />
-          Working with interface design in Figma
-        </p>
+          ))}
+          <h4 className="text-2xl font-bold">
+            Additional proficiencies include
+          </h4>
+          <p className="text-lg">
+            Editing images with Gimp
+            <br />
+            Working with interface design in Figma
+          </p>
+        </div>
       </div>
       <HrAnimation width="50%" />
     </>
---
+++ src/components/useInView.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef, useState } from 'react';
+
+function useInView(threshold: number) {
+  const [inView, setInView] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setInView(entry.isIntersecting);
+      },
+      { threshold }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref, threshold]);
+
+  return { ref, inView };
+}
+
+export default useInView;
