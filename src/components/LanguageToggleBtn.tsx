90d7e36e364be3d744f65740e88c63d712b3090a
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-03-25 16:08:33 +0100

    Removed some code

--- src/components/LanguageToggleBtn.tsx
+++ src/components/LanguageToggleBtn.tsx
@@ -1,16 +1,13 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 
 function LanguageToggle() {
   const { i18n } = useTranslation();
   const currentLanguage = i18n.language;
-  const [isActive, setIsActive] = useState(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
 
   const changeLanguage = (lang: string) => {
     i18n.changeLanguage(lang);
-    setIsActive(!isActive);
-
     buttonRef.current?.blur();
   };
 
@@ -18,9 +15,7 @@ function LanguageToggle() {
     <div className="absolute top-0 left-0 font-extrabold text-lg">
       <button
         ref={buttonRef}
-        className={`flex gap-2 place-items-center rounded-xl p-2  hover:bg-[#00d1ceb4] ${
-          isActive ? '' : ' focus:bg-[#00d1ceb4]'
-        } focus:outline-none`}
+        className={`flex gap-2 place-items-center rounded-xl p-2  hover:bg-[#00d1ceb4] focus:bg-[#00d1ceb4] focus:outline-none`}
         onClick={() => changeLanguage(currentLanguage === 'en' ? 'sv' : 'en')}
         title={currentLanguage === 'en' ? 'Byt språk' : 'Change language'}
       >
