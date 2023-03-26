5d51ea8fec7ca23a0a624757ae0008e657989345
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-03-26 13:07:12 +0200

    adjustment to button behaviour on mobile

--- src/components/LanguageToggleBtn.tsx
+++ src/components/LanguageToggleBtn.tsx
@@ -1,21 +1,34 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 function LanguageToggle() {
   const { i18n } = useTranslation();
   const currentLanguage = i18n.language;
+  const [btnFocusedByTab, setBtnFocusedByTab] = useState(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
 
   const changeLanguage = (lang: string) => {
     i18n.changeLanguage(lang);
   };
 
+  const handleBtnTab = (event: React.KeyboardEvent<HTMLButtonElement>) => {
+    if (event.key === 'Tab') {
+      setBtnFocusedByTab(true);
+    }
+  };
+
   return (
     <div className="absolute top-0 left-0 font-extrabold text-lg">
       <button
         ref={buttonRef}
-        className={`flex gap-2 place-items-center rounded-xl p-2 active:bg-none hover:bg-[#00d1ceb4] sm:active:bg-[#00d1ceb4]  sm:active:outline-none`}
+        className={`flex gap-2 place-items-center rounded-xl p-2 focus:bg-none ${
+          btnFocusedByTab
+            ? 'focus:text-[#00d1ceb4] focus:outline-none'
+            : 'focus:outline-none'
+        }`}
         onClick={() => changeLanguage(currentLanguage === 'en' ? 'sv' : 'en')}
+        onKeyUp={handleBtnTab}
+        onBlur={() => setBtnFocusedByTab(false)}
         title={currentLanguage === 'en' ? 'Byt språk' : 'Change language'}
       >
         <i className="fa-solid fa-earth-europe fa-lg" />
