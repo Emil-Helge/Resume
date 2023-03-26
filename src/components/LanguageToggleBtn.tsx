5d4cda5abe7aa176dd21844faefc26e45d2dc811
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-03-26 13:46:42 +0200

    border tweak

--- src/components/LanguageToggleBtn.tsx
+++ src/components/LanguageToggleBtn.tsx
@@ -18,10 +18,10 @@ function LanguageToggle() {
   };
 
   return (
-    <div className="absolute top-2 left-2 font-extrabold text-lg">
+    <div className="absolute top-1 left-1 font-extrabold text-lg border border-slate-800 p-1 rounded-full">
       <button
         ref={buttonRef}
-        className={`flex gap-2 place-items-center rounded-xl active:text-[#00d1ce] active:bg-none focus:bg-none ${
+        className={`flex gap-2 place-items-center active:text-[#00d1ce] active:bg-none focus:bg-none ${
           btnFocusedByTab
             ? 'focus:text-[#00d1ce] focus:outline-none'
             : 'focus:outline-none'
