1bb1257d9a17100861009fa93c495be07a709b62
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-03-26 14:56:59 +0200

    attempt at fixing mobile highlight on tap

--- src/components/LanguageToggleBtn.tsx
+++ src/components/LanguageToggleBtn.tsx
@@ -20,9 +20,9 @@ function LanguageToggle() {
   return (
     <button
       ref={buttonRef}
-      className={`absolute top-1 left-1 font-extrabold text-lg flex gap-2 place-items-center select-none active:bg-[#00d1ce] border border-slate-800 rounded-full p-1 ${
+      className={`absolute top-1 left-1 font-extrabold text-lg flex gap-2 place-items-center select-none active:text-[#00d1ce] active:bg-slate-800 border border-slate-800 rounded-full p-1 ${
         btnFocusedByTab
-          ? 'focus:text-[#00d1ce] focus:outline-none'
+          ? 'focus:text-[#00d1ceb4] focus:outline-none'
           : 'focus:outline-none'
       }`}
       onClick={() => changeLanguage(currentLanguage === 'en' ? 'sv' : 'en')}
